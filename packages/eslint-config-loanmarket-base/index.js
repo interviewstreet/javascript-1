8aec06d6f7d1b47587a25d63cf27a8d67463facd
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2017-09-29 16:30:13 +1000

    setup initial base eslint config

---
+++ packages/eslint-config-loanmarket-base/index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "airbnb-base"
+  ],
+  "env": {
+    "es6": true,
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+    },
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-mixed-operators": ["error", { "allowSamePrecedence": true }],
+    "arrow-body-style": 0,
+    "no-fallthrough": ["error", { "commentPattern": "break[\\s\\w]*omitted" }],
+    "no-param-reassign": ["error", { "props": false } ],
+    "class-methods-use-this": ["error", { "exceptMethods": ["getChildContext", "componentDidMount", "componentWillMount", "componentWillUpdate", "componentWillReceiveProps"] }],
+    "no-underscore-dangle": 0,
+    "react/forbid-prop-types": [
+      2, { "forbid": ["any", "array"] }
+    ],
+    "new-cap": ["error", {
+      "capIsNew": false
+    }],
+    "func-names": 0,
+    "import/prefer-default-export": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "max-len": [0, { "code": 120, "ignoreStrings": true }],
+    "curly": 0,
+    "indent": [
+      "error", 2
+    ],
+    "linebreak-style": [
+      "error", "unix"
+    ],
+    "quotes": [
+      "warn", "single", {"avoidEscape": true, "allowTemplateLiterals": true}
+    ],
+    "semi": [
+      "error", "always"
+    ],
+    "no-console": 0,
+    "consistent-return": 0,
+    "no-unused-expressions": [
+      "error", { "allowShortCircuit": true, "allowTernary": false }
+    ],
+    "no-plusplus": 0
+  }
+};
